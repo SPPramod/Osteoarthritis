8b2363ae84b658eb925599bf7e680e6506599045
Sathya Pushana Pramod <pushanapramod@gmail.com>
2025-05-14 18:18:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ if uploaded_file is not None:
     st.image(img, channels="BGR", caption="Uploaded Image", use_container_width=True)
 
     if st.button("Predict"):
-        label_index, prediction_text, processed_img = predict_label(img)
+        prediction_text, processed_img = predict_label(img)
         st.success(f"Prediction: **{prediction_text}**")
 
         # Create subplot-like visualization
